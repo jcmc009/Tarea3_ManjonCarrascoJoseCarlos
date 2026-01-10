0f32b78baee85e3dc24adeaa5ba04947f1448270
Jc <jc0.1@hotm,ai>
2026-01-10 19:11:35 +0100

    incremento y navHost

--- app/src/main/java/com/example/tarea3_manjoncarrascojosecarlos/BlankFragment.kt
+++ app/src/main/java/com/example/tarea3_manjoncarrascojosecarlos/BlankFragment.kt
@@ -5,11 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
-import androidx.navigation.fragment.findNavController
-import com.example.tarea3_manjoncarrascojosecarlos.databinding.FragmentLoginBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Login.newInstance] factory method to
+ * Use the [BlankFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Login : Fragment() {
+class BlankFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -37,11 +32,9 @@ class Login : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
-
+        return inflater.inflate(R.layout.fragment_blank, container, false)
     }
 
     companion object {
@@ -51,24 +44,16 @@ class Login : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Login.
+         * @return A new instance of fragment BlankFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Login().apply {
+            BlankFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
     }
-
-    private var _binding: FragmentLoginBinding? = null
-    private val binding get() = _binding!!
-
-
-
-
-
 }
